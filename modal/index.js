9637e6ae6e3dbb6c3e15e1a68848845cfed77de8
shweta <shwetakodamg@gmail.com>
2022-08-11 18:06:15 +0530

    project

--- modal/index.js
+++ modal/index.js
@@ -1,6 +1,20 @@
 //selectors
 
 let modal = document.querySelector(".modal")
-let closebtn = document.querySelector(".closebtn")
 let clickbtn = document.querySelector(".clickbtn")
+let closebtn = document.querySelector(".closebtn")
+let submitbtn = document.querySelector(".submit")
 
+//function
+
+clickbtn.addEventListener("click", openmodal)
+closebtn.addEventListener("click", closemodal)
+submitbtn.addEventListener("click", closemodal)
+
+function openmodal(){
+    modal.style.display = "block"
+}
+
+function closemodal(){
+    modal.style.display = "none"
+}
