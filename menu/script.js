a084462500d8efb59b81d40ecd9ed6d2fc534fb9
shweta <shwetakodamg@gmail.com>
2022-08-16 23:22:37 +0530

    project

---
+++ menu/script.js
@@ -0,0 +1,51 @@
+//selectors
+let all =document.querySelector(".all")
+let brkfst =document.querySelector(".brkfst")
+let lunch =document.querySelector(".lunch")
+let snacks =document.querySelector(".snacks")
+let dinner =document.querySelector(".dinner")
+
+let brkfstBox =document.querySelector(".brkfstBox")
+let lunchBox =document.querySelector(".lunchBox")
+let snacksBox =document.querySelector(".snacksBox")
+let dinnerBox =document.querySelector(".dinnerBox")
+
+all.addEventListener("click", function (){
+    brkfstBox.style.display = "inline-block";
+    lunchBox.style.display = "inline-block"
+    snacksBox.style.display = "inline-block"
+    dinnerBox.style.display = "inline-block"
+
+})
+
+
+
+
+brkfst.addEventListener("click", function (){
+    brkfstBox.style.display = "inline-block";
+    lunchBox.style.display = "none"
+    snacksBox.style.display = "none"
+    dinnerBox.style.display = "none"
+})
+
+
+lunch.addEventListener("click", function (){
+    brkfstBox.style.display = "none";
+    lunchBox.style.display = "inline-block"
+    snacksBox.style.display = "none"
+    dinnerBox.style.display = "none"
+})
+
+snacks.addEventListener("click", function (){
+    brkfstBox.style.display = "none";
+    lunchBox.style.display = "none"
+    snacksBox.style.display = "inline-block"
+    dinnerBox.style.display = "none"
+})
+
+dinner.addEventListener("click", function (){
+    brkfstBox.style.display = "none";
+    lunchBox.style.display = "none"
+    snacksBox.style.display = "none"
+    dinnerBox.style.display = "inline-block"
+})
--- questions/script.js
+++ questions/script.js
@@ -2,8 +2,9 @@
 
 let accordion = document.querySelectorAll(".box")
 
-for(let i = 0; i < accordion.length; i++) {
+for(let i = 0; i <accordion.length; i++) {
     accordion[i].addEventListener("click", function() {
+        // e.preventDefault()
         this.classList.toggle("active");
     })
 }
