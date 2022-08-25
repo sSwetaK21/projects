3e53fbecfdaf126898900033d90a2c841542c79b
shweta <shwetakodamg@gmail.com>
2022-08-25 23:25:54 +0530

    project

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -4,17 +4,40 @@ let music = new Audio("music.mp3")
 let turn_audio = new Audio("ting.mp3")
 let gameover = new Audio("gameover.mp3")
 let turn = "X"
-// let isgameover = false
+let isgameover = false
+
 // function changing turn value
 
 function changeTurn(){
     if(turn === "X"){
+        // turn.style.color="red"
+        // turn ="0"
         return "0"
     }else{
         return "X"
     }
 }
 function checkWin(){
+    let boxtext2 = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0, 1, 2, 5, 5, 0],
+        [3, 4, 5, 5, 15, 0],
+        [6, 7, 8, 5, 25, 0],
+        [0, 3, 6, -5, 15, 90],
+        [1, 4, 7, 5, 15, 90],
+        [2, 5, 8, 15, 15, 90],
+        [0, 4, 8, 5, 15, 45],
+        [2, 4, 6, 5, 15, 135],
+    ]
+    wins.forEach(e=>{
+        if((boxtext2[e[0]].innerText === boxtext2[e[1]].innerText)  && (boxtext2[e[2]].innerText === boxtext2[e[1]].innerText) && (boxtext2[e[0]].innerText !== "")){
+            document.querySelector(".info").innerText = boxtext2[e[0]].innerText + " Won!";
+            isgameover = true;
+            gameover.play()
+            document.querySelector(".imgbox").getElementsByTagName("img")[0].style.width = "200px";
+
+        }
+    })
 
 }
 
@@ -30,9 +53,9 @@ Array.from(boxes).forEach(element =>{
             turn = changeTurn();
             turn_audio.play();
             checkWin();
-            // if (!isgameover){
+            if (!isgameover){
                 document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
-            // } 
+            } 
         }
     })
 })
