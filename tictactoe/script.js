a945cb1b8b0981ff27652ef58a00cb4527d5f3a9
shweta <shwetakodamg@gmail.com>
2022-08-25 22:02:49 +0530

    project

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -4,7 +4,7 @@ let music = new Audio("music.mp3")
 let turn_audio = new Audio("ting.mp3")
 let gameover = new Audio("gameover.mp3")
 let turn = "X"
-let isgameover = false
+// let isgameover = false
 // function changing turn value
 
 function changeTurn(){
@@ -14,21 +14,39 @@ function changeTurn(){
         return "X"
     }
 }
+function checkWin(){
 
+}
 
 // main logic
 
+// let boxes = document.getElementsByClassName("box");
+// Array.from(boxes).forEach(ele=>{
+//     let boxtext = ele.querySelector(".boxtext");
+//     ele.addEventListener("click", ()=>{
+//         if(boxtext.innertext === ""){
+//             boxtext.innerText = turn;
+//             turn =changeTurn();
+//             turn_audio.play();
+//             // if (!isgameover){
+//                 document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
+//             // } 
+//         }
+//     })
+// })
+
 let boxes = document.getElementsByClassName("box");
-Array.from(boxes).forEach(e=>{
-    let boxtext = e.querySelector(".boxtext");
-    e.addEventListener("click", ()=>{
-        if(boxtext === ""){
-            boxtext.innerText = turn
-            turn =changeTurn()
-            turn_audio.play()
-            if (!isgameover){
+Array.from(boxes).forEach(element =>{
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', ()=>{
+        if(boxtext.innerText === ''){
+            boxtext.innerText = turn;
+            turn = changeTurn();
+            turn_audio.play();
+            checkWin();
+            // if (!isgameover){
                 document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
-            } 
+            // } 
         }
     })
 })
